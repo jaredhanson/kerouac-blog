bce210274c7439adfc966ae3323d9a27270f62ab
Jared Hanson <jaredhanson@gmail.com>
2017-10-06 18:41:08 -0700

    Add test case for atom feed.

---
+++ test/handlers/feed/atom.test.js
@@ -0,0 +1,47 @@
+var chai = require('chai');
+var kerouac = require('kerouac')
+var handler = require('../../../lib/handlers/feed/atom');
+
+
+describe('handlers/feed/atom', function() {
+  var site = kerouac();
+  
+  describe('with one post', function() {
+    var page, err;
+
+    before(function(done) {
+      chai.kerouac.handler(handler())
+        .page(function(page) {
+          page.site = site;
+          page.pages = [
+            { url: '/blog/2017/09/03/hello/',
+              post: true,
+              title: 'Hello, World',
+              createdAt: new Date(Date.UTC(2017, 8, 3, 17, 30, 15)) }
+          ];
+        })
+        .end(function(p) {
+          page = p;
+          done();
+        })
+        .dispatch();
+    });
+  
+    it('should write feed', function() {
+      var expected = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<feed xmlns=\"http://www.w3.org/2005/Atom\">',
+        '  <entry>',
+        '    <title>Hello, World</title>',
+        '    <link href=\"/blog/2017/09/03/hello/\"/>',
+        '    <published>2017-09-03T17:30:15Z</published>',
+        '  </entry>',
+        '</feed>',
+        ''
+      ].join("\n");
+      
+      expect(page.body).to.equal(expected);
+    });
+  }); // with one post
+  
+});
