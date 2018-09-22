ab6834a41a90a1983715bd4d8f550600ecca7417
Jared Hanson <jaredhanson@gmail.com>
2018-09-22 15:23:11 -0700

    Remove dead code.

--- app/site.js
+++ app/site.js
@@ -75,59 +75,6 @@ exports = module.exports = function(
       
       done();
     });
-    
-    return;
-    
-    
-    fs.readdir(dir, function(err, files) {
-      if (err && err.code == 'ENOENT') {
-        return done();
-      } else if (err) { return done(err); }
-      
-      var idx = 0
-        , file, base, ext
-        , url;
-      
-      (function iter(err) {
-        if (err) { return done(err); }
-        
-        file = files[idx++];
-        if (!file) {
-          return done();
-        }
-        
-        file = path.join(dir, file);
-        ext = path.extname(file);
-        base = path.basename(file, ext);
-        
-        console.log('GOT FILE!');
-        console.log(file);
-        
-        fs.stat(file, function(err, stats) {
-          if (err) { return iter(err); }
-          if (!stats.isFile()) { return iter(); }
-          
-          var year = stats.birthtime.getUTCFullYear()
-            , month = (stats.birthtime.getUTCMonth() + 1)
-            , day = stats.birthtime.getUTCDate()
-            , slug = base;
-          month = (month < 10) ? ('0' + month) : month;
-          day = (day < 10) ? ('0' + day) : day;
-          
-          var match;
-          if (match = DASHED_REGEX.exec(base)) {
-            year = match[1];
-            month = match[2];
-            day = match[3];
-            slug = match[4];
-          }
-          
-          var url = '/' + [ year, month, day, slug + '.html' ].join('/');
-          self.add(url);
-          iter();
-        });
-      })();
-    });
   });
   
   return site;
