695398619323e5b97a170a0497ca7de2295fe0ea
Jared Hanson <jaredhanson@gmail.com>
2018-09-23 10:11:34 -0700

    Add files.

---
+++ test/lib/localpostsdatabase.test.js
@@ -0,0 +1,47 @@
+var $require = require('proxyquire');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var LocalPostsDatabase = require('../../lib/localpostsdatabase');
+
+
+describe('LocalPostsDatabase', function() {
+  
+  it('should export constructor', function() {
+    expect(LocalPostsDatabase).to.be.a('function');
+  });
+  
+  describe('with Jekyll-style file system layout', function() {
+    var db = new LocalPostsDatabase('test/fixtures/date');
+  
+    describe('#list', function() {
+      var posts;
+    
+      before(function(done) {
+        db.list(function(err, p) {
+          if (err) { return done(err); }
+          posts = p;
+          return done();
+        });
+      });
+    
+      it('should queue pages', function() {
+        expect(posts).to.have.length(2);
+
+        expect(posts[0].publishedAt).to.be.an.instanceof(Date);
+        delete posts[0].publishedAt;
+        expect(posts[0]).to.deep.equal({
+          slug: '2017-09-03-hello',
+        });
+        
+        expect(posts[1].publishedAt).to.be.an.instanceof(Date);
+        delete posts[1].publishedAt;
+        expect(posts[1]).to.deep.equal({
+          slug: '2017-09-04-hello-again',
+        })
+      });
+    
+    });
+  
+  });
+  
+});
