d3a004f34580a03d220cd0011b888b69bbc40042
Jared Hanson <jaredhanson@gmail.com>
2018-09-25 18:20:33 -0700

    Add tests.

--- test/handlers/feed/rdf.test.js
+++ test/handlers/feed/rdf.test.js
@@ -63,6 +63,60 @@ describe('handlers/feed/rdf', function() {
       });
     }); // with one post
     
+    describe('generating a feed with the sample from RSS 1.0 Specification', function() {
+      // http://web.resource.org/rss/1.0/spec#s4.1
+      
+      var site = kerouac();
+      site.set('title', 'XML.com');
+      site.set('description', 'XML.com features a rich mix of information and services for the XML community.');
+      
+      var page, err;
+
+      before(function(done) {
+        chai.kerouac.handler(factory())
+          .page(function(page) {
+            page.canonicalURL = 'http://www.example.com/blog/feed.rdf';
+            
+            page.site = site;
+            page.site.pages = [
+              { url: '/2003/12/13/hello-world/',
+                canonicalURL: 'http://www.example.com/blog/2003/12/13/hello-world/',
+                meta: { post: true },
+                locals: {
+                  title: 'Hello, World',
+                  publishedAt: new Date('2003-12-13T18:30:02Z'),
+                }
+              }
+            ];
+          })
+          .end(function(p) {
+            page = p;
+            done();
+          })
+          .dispatch();
+      });
+  
+      it('should write feed', function() {
+        var expected = [
+          '<?xml version="1.0" encoding="UTF-8"?>',
+          '<rdf:RDF xmlns="http://purl.org/rss/1.0/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">',
+          '  <channel>',
+          '    <title>XML.com</title>',
+          '    <description>XML.com features a rich mix of information and services for the XML community.</description>',
+          '  </channel>',
+          '  <item>',
+          '    <title>Hello, World</title>',
+          '    <link>http://www.example.com/blog/2003/12/13/hello-world/</link>',
+          '    <dc:date>2003-12-13T18:30:02Z</dc:date>',
+          '  </item>',
+          '</rdf:RDF>',
+          ''
+        ].join("\n");
+      
+        expect(page.body).to.equal(expected);
+      });
+    }); // generating a feed with the sample from RSS 1.0 Specification
+    
   }); // handler
   
 });
