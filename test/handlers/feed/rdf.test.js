a5174f506a0dac71d696bb6ced505dafb7652fc7
Jared Hanson <jaredhanson@gmail.com>
2018-09-26 17:38:24 -0700

    Update tests.

--- test/handlers/feed/rdf.test.js
+++ test/handlers/feed/rdf.test.js
@@ -69,7 +69,7 @@ describe('handlers/feed/rdf', function() {
       });
     }); // with one post
     
-    describe('generating a feed with basid document example from RSS 1.0 Specification', function() {
+    describe('generating a feed with basic document example from RSS 1.0 Specification', function() {
       // http://web.resource.org/rss/1.0/spec#s4.1
       
       var site = kerouac();
@@ -131,7 +131,71 @@ describe('handlers/feed/rdf', function() {
       
         expect(page.body).to.equal(expected);
       });
-    }); // generating a feed with basid document example from RSS 1.0 Specification
+    }); // generating a feed with basic document example from RSS 1.0 Specification
+    
+    describe('generating a feed with document pulling in modules example from RSS 1.0 Specification', function() {
+      // http://web.resource.org/rss/1.0/spec#s4.1
+      
+      var site = kerouac();
+      site.set('title', 'Meerkat');
+      site.set('description', 'Meerkat: An Open Wire Service');
+      
+      var page, err;
+
+      before(function(done) {
+        chai.kerouac.handler(factory())
+          .page(function(page) {
+            page.canonicalURL = 'http://meerkat.oreillynet.com/?_fl=rss1.0';
+            
+            page.site = site;
+            page.site.pages = [
+              { url: '/',
+                canonicalURL: 'http://meerkat.oreillynet.com',
+                meta: { home: true },
+                locals: {
+                }
+              },
+              { url: '/2000/08/09/xslt/xslt.html',
+                canonicalURL: 'http://xml.com/pub/2000/08/09/xslt/xslt.html',
+                meta: { post: true },
+                locals: {
+                  title: 'Processing Inclusions with XSLT',
+                }
+              }
+            ];
+          })
+          .end(function(p) {
+            page = p;
+            done();
+          })
+          .dispatch();
+      });
+  
+      it('should write feed', function() {
+        var expected = [
+          '<?xml version="1.0" encoding="UTF-8"?>',
+          '<rdf:RDF xmlns="http://purl.org/rss/1.0/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">',
+          '  <channel rdf:about="http://meerkat.oreillynet.com/?_fl=rss1.0">',
+          '    <title>Meerkat</title>',
+          '    <description>Meerkat: An Open Wire Service</description>',
+          '    <link>http://meerkat.oreillynet.com</link>',
+          '    <items>',
+          '      <rdf:Seq>',
+          '        <rdf:li resource="http://xml.com/pub/2000/08/09/xslt/xslt.html"/>',
+          '      </rdf:Seq>',
+          '    </items>',
+          '  </channel>',
+          '  <item rdf:about="http://xml.com/pub/2000/08/09/xslt/xslt.html">',
+          '    <title>Processing Inclusions with XSLT</title>',
+          '    <link>http://xml.com/pub/2000/08/09/xslt/xslt.html</link>',
+          '  </item>',
+          '</rdf:RDF>',
+          ''
+        ].join("\n");
+      
+        expect(page.body).to.equal(expected);
+      });
+    }); // generating a feed with document pulling in modules example from RSS 1.0 Specification
     
   }); // handler
   
