0992bbc5ae813de98ff03361b207034d1db63afe
Jared Hanson <jaredhanson@gmail.com>
2023-09-15 12:00:01 -0700

    Clean up tests.

--- test/blog.test.js
+++ test/blog.test.js
@@ -12,7 +12,7 @@ describe('Blog', function() {
   
   describe('#entries', function() {
     
-    it('should yield entries', function(done) {
+    it('should yield entries in UTC timezone', function(done) {
       var blog = new Blog('test/fixtures/date');
       
       blog.entries(function(err, entries) {
@@ -34,45 +34,6 @@ describe('Blog', function() {
         done();
       });
     });
-    
-    
-    
-    describe('in UTC timezone', function() {
-      var db = new Blog('test/fixtures/date');
-  
-      describe('#list', function() {
-        var posts;
-    
-        before(function(done) {
-          db.entries(function(err, p) {
-            if (err) { return done(err); }
-            posts = p;
-            return done();
-          });
-        });
-    
-        it('should queue pages', function() {
-          expect(posts).to.have.length(3);
-          expect(posts[0]).to.deep.equal({
-            slug: 'hello',
-            publishedAt: new Date('2017-09-03T00:00:00.000Z'),
-            path: '2017-09-03-hello.md'
-          });
-          expect(posts[1]).to.deep.equal({
-            slug: 'hello-again',
-            publishedAt: new Date('2017-09-04T00:00:00.000Z'),
-            path: '2017-09-04-hello-again.md'
-          });
-          expect(posts[2]).to.deep.equal({
-            slug: 'published',
-            publishedAt: new Date('2018-04-26T20:09:27.000Z'),
-            path: '2018-04-22-published.md'
-          });
-        }); 
-      }); // #list
-      
-    }); // in UTC timezone
-    
     
   });
   
