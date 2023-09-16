6676b615bfefe2700eae74c32a662c9004c07b1e
Jared Hanson <jaredhanson@gmail.com>
2023-09-16 06:29:27 -0700

    Add tests.

--- test/handlers/post.test.js
+++ test/handlers/post.test.js
@@ -7,6 +7,45 @@ var factory = require('../../lib/handlers/post');
 
 describe('handlers/post', function() {
   
+  it('should render post', function(done) {
+    var blog = new Object();
+    blog.entry = sinon.stub().yields(null, {
+      title: 'Hello, World',
+      content: 'This post was written using Markdown.',
+      format: 'md',
+      publishedAt: new Date(Date.UTC(2003, 11, 13, 18, 30, 2)),
+      modifiedAt: new Date(Date.UTC(2005, 6, 31, 12, 29, 29))
+    });
+    
+    chai.kerouac.page(factory(blog))
+      .request(function(page) {
+        //page.site = site;
+        page.params = { slug: 'hello' };
+      })
+      .finish(function(p, l) {
+        expect(blog.entry.callCount).to.equal(1);
+        var call = blog.entry.getCall(0)
+        expect(call.args[0]).to.deep.equal({
+          slug: 'hello',
+          year: undefined,
+          month: undefined,
+          day: undefined
+        });
+        
+        
+        expect(this).to.render('blog/post')
+          .and.beginWith.content('This post was written using Markdown.').of.format('md');
+        
+        //page = this;
+        //layout = l;
+        done();
+      })
+      .generate();
+    
+  });
+  
+  
+  
   describe('handler', function() {
     var site = kerouac();
     
@@ -52,8 +91,7 @@ describe('handlers/post', function() {
           slug: 'hello',
           year: undefined,
           month: undefined,
-          day: undefined,
-          file: 'hello.md'
+          day: undefined
         });
       });
       
