e192e50edb797aa0bc01dee5eee448c9201a564f
Jared Hanson <jaredhanson@gmail.com>
2023-09-16 06:58:42 -0700

    Clean up tests.

--- test/handlers/post.test.js
+++ test/handlers/post.test.js
@@ -35,67 +35,15 @@ describe('handlers/post', function() {
           .and.beginWith.content('This post was written using Markdown.').of.format('md');
         
         expect(this.isPost).to.be.true;
-        
-        //page = this;
-        //layout = l;
+        expect(this.locals).to.deep.equal({
+          title: 'Hello, World',
+          publishedAt: new Date('2003-12-13T18:30:02Z'),
+          modifiedAt: new Date('2005-07-31T12:29:29Z')
+        });
         done();
       })
       .generate();
     
   });
-  
-  
-  
-  describe('handler', function() {
-    var site = kerouac();
-    
-    var postsDB = {
-      entry: function(){}
-    };
-    
-    describe('with slug as only parameter', function() {
-      var page, layout, err;
-      
-      before(function() {
-        sinon.stub(postsDB, 'entry').yields(null, {
-          title: 'Hello, World',
-          content: 'This post was written using Markdown.',
-          publishedAt: new Date(Date.UTC(2003, 11, 13, 18, 30, 2)),
-          modifiedAt: new Date(Date.UTC(2005, 6, 31, 12, 29, 29))
-        });
-      });
-    
-      after(function() {
-        postsDB.entry.restore();
-      });
-      
-      before(function(done) {
-        chai.kerouac.page(factory(postsDB))
-          .request(function(page) {
-            page.site = site;
-            page.params = { slug: 'hello' };
-            page.context = { file: 'hello.md' }
-          })
-          .finish(function(p, l) {
-            page = this;
-            layout = l;
-            done();
-          })
-          .generate();
-      });
-      
-      /*
-      it('should set locals', function() {
-        expect(page.locals).to.deep.equal({
-          title: 'Hello, World',
-          content: '<p>This post was written using Markdown.</p>\n',
-          publishedAt: new Date('2003-12-13T18:30:02Z'),
-          modifiedAt: new Date('2005-07-31T12:29:29Z')
-        });
-      });
-      */
-    }); // with slug as only parameter
-    
-  }); // handler
   
 });
