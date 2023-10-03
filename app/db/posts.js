45044a09b82ad01caa532aebd45ed40386003333
Jared Hanson <jaredhanson@gmail.com>
2023-10-03 16:53:38 -0700

    Remove unused file.

--- app/db/posts.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function() {
-  var LocalPostsDatabase = require('../../lib/localpostsdatabase');
-  
-  return new LocalPostsDatabase();
-};
-
-exports['@implements'] = 'http://i.kerouacjs.org/blog/PostsDatabase';
-exports['@singleton'] = true;
-exports['@require'] = [];
