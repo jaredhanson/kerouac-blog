f60d4574d6014431cd810d091fa3a9929569f8ad
Jared Hanson <jaredhanson@gmail.com>
2023-10-10 20:44:52 -0700

    Clean up.

--- test/handlers/feed/rdf.test.js
+++ test/handlers/feed/rdf.test.js
@@ -77,59 +77,6 @@ describe('handlers/feed/rdf', function() {
   
   
   describe.skip('handler', function() {
-  
-    describe('with one post', function() {
-      var site = kerouac();
-      
-      var page, err;
-
-      before(function(done) {
-        chai.kerouac.page(factory())
-          .request(function(page) {
-            page.canonicalURL = 'http://www.example.com/blog/feed.rdf';
-            
-            page.site = site;
-            page.site.pages = [
-              { url: '/2003/12/13/hello-world/',
-                canonicalURL: 'http://www.example.com/blog/2003/12/13/hello-world/',
-                meta: { post: true },
-                locals: {
-                  title: 'Hello, World',
-                  publishedAt: new Date('2003-12-13T18:30:02Z'),
-                }
-              }
-            ];
-          })
-          .finish(function() {
-            page = this;
-            done();
-          })
-          .generate();
-      });
-  
-      it('should write feed', function() {
-        var expected = [
-          '<?xml version="1.0" encoding="UTF-8"?>',
-          '<rdf:RDF xmlns="http://purl.org/rss/1.0/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">',
-          '  <channel rdf:about="http://www.example.com/blog/feed.rdf">',
-          '    <items>',
-          '      <rdf:Seq>',
-          '        <rdf:li resource="http://www.example.com/blog/2003/12/13/hello-world/"/>',
-          '      </rdf:Seq>',
-          '    </items>',
-          '  </channel>',
-          '  <item rdf:about="http://www.example.com/blog/2003/12/13/hello-world/">',
-          '    <title>Hello, World</title>',
-          '    <link>http://www.example.com/blog/2003/12/13/hello-world/</link>',
-          '    <dc:date>2003-12-13T18:30:02Z</dc:date>',
-          '  </item>',
-          '</rdf:RDF>',
-          ''
-        ].join("\n");
-      
-        expect(page.body).to.equal(expected);
-      });
-    }); // with one post
     
     describe('generating a feed with basic document example from RSS 1.0 Specification', function() {
       // http://web.resource.org/rss/1.0/spec#s4.1
